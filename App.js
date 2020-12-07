05ce6120dbfbaff2069c72f4bd5c731f6b1f9a67
Michał Poręba <michalporeba@gmail.com>
2020-12-07 23:15:09 +0000

    following the basic tutorial of React Native

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [isSomething, setIsSomething] = useState(true);
+  const [searchText, setSearchText] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>FRApp</Text>
+      <TextInput style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        placeholder="Search"
+        onChangeText={text => {setSearchText(text)}}
+        />  
+      <Button title="New Patient" />
+      <Button title="Primary Survey" 
+        onPress={() => {setIsSomething(!isSomething)}} />
+      <Button title="Secondary Survey" 
+        disabled={!isSomething}/>
+      <Button title="Others" />
+      <Text>{searchText}</Text>
       <StatusBar style="auto" />
     </View>
   );
