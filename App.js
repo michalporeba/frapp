f807cdbc5244ba82d65faa5a6a2b279f10830c54
Michał Poręba <michalporeba@gmail.com>
2020-12-08 00:43:06 +0000

    looking at styling

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
+// import * as eva from '@eva-design/eva';
+// import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+// import { Container } from 'native-base';
+import * as Font from 'expo-font'
+import { Ionicons } from '@expo/vector-icons';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+// const HomeScreen = () => (
+//   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+//     <Text category='h1'>HOME</Text>
+//   </Layout>
+// );
+
+// export default () => (
+//   <ApplicationProvider {...eva} theme={eva.light}>
+//     <HomeScreen />
+//   </ApplicationProvider>
+// );
 
 export default function App() {
   const [isSomething, setIsSomething] = useState(true);
@@ -31,7 +48,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffffe',
     alignItems: 'center',
     justifyContent: 'center',
   },
