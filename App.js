5fa776f8e77ca3faab58a68f8caccaca454698d6
Michał Poręba <michalporeba@gmail.com>
2020-12-10 19:04:29 +0000

    following https://www.youtube.com/watch?v=0-S5a0eXPoc to set up basic UI

--- App.js
+++ App.js
@@ -2,15 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import * as Font from 'expo-font'
 import { Ionicons } from '@expo/vector-icons';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Image, Platform, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 import { Button, Icon } from 'react-native-elements';
+import MainScreen from './app/screens/MainScreen';
 
 export default function App() {
   const [isSomething, setIsSomething] = useState(true);
   const [searchText, setSearchText] = useState('');
+
+  console.log("testing messages");
+  let x = 1;
+  console.log("executed");
+
   return (
-    <View style={styles.container}>
-      <Text>FRApp</Text>
+    // <SafeAreaView style={styles.container}>
+    <SafeAreaView style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        justifyContent: "space-around"
+    }}>
+      <MainScreen />
+
+      {/* <Image source={require("./assets/caverescue.jpg")} 
+         style={{ height: 200, width: 200}}
+      />
+      <Text>FRAppss</Text>
       <TextInput style={{
           height: 40,
           borderColor: 'gray',
@@ -19,7 +35,7 @@ export default function App() {
         placeholder="Search"
         onChangeText={text => {setSearchText(text)}}
         />  
-      <Button title="New Patient" raised="true" type="outline" />
+      <Button title="New Patient" type="outline" />
       <Button title="Primary Survey" 
         onPress={() => {setIsSomething(!isSomething)}} />
       <Button title="Secondary Survey" 
@@ -31,17 +47,24 @@ export default function App() {
         containerStyle={{ margin: 5 }}
         icon={<Icon name="devices" size={50} color="#0F0" />}
         title="hello"
-      /> 
+        onPress={() => 
+          Alert.alert("Custom alert", "A question", [
+            { text: "Yes", onPress: () => console.log("yes")},
+            { text: "No", onPress: () => console.log("no")}
+          ])
+        }
+      />  */}
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fffffe',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    paddingTop: 20
+  }
 });
---
+++ app/screens/MainScreen.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Alert, Image, Platform, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
+
+import colors from '../config/colors'
+
+function MainScreen(props) {
+    return (
+        <>
+        <View style={{
+            backgroundColor: "gold",
+            flex: 0.5
+          }}
+          />
+          <View style={{
+            backgroundColor: "dodgerblue",
+            flex: 3
+          }}
+          />
+    
+          <View style={{
+            backgroundColor: "gold",
+            flex: 1
+          }}
+          />
+    
+          <View style={{
+            backgroundColor: "green",
+            flex: 3,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            alignContent: "space-around",
+            flexWrap: "wrap",
+          
+          }}
+          >
+            <View style={styles.buttonView}>
+               <Button buttonStyle={styles.button} title="One" />
+            </View>
+    
+            <View style={styles.buttonView}>
+               <Button buttonStyle={styles.button} title="Two" />
+            </View>
+            
+            <View style={styles.buttonView}>
+               <Button buttonStyle={styles.button} title="Three" />
+            </View>
+    
+            <View style={styles.buttonView}>
+               <Button buttonStyle={styles.button} title="Four" />
+            </View>
+          </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    buttonView: {
+      width: '40%',
+      height: '40%',
+    },
+    button: {
+      width: '100%',
+      height: '100%',
+      backgroundColor: colors.primary
+    }
+  });
+
+  
+export default MainScreen;
