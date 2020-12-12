a1a31e416414b19f4f8ea1a3cff67ac7404cbd4a
Michał Poręba <michalporeba@gmail.com>
2020-12-12 16:22:20 +0000

    setting up the layout

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Image, Picker, Platform, StyleSheet, Text, TextInput, View, I18nManager } from 'react-native';
+import { Image, ImageBackground, Picker, Platform, StyleSheet, Text, TextInput, View, I18nManager } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import I18n from 'i18n-js';
 
@@ -8,6 +8,7 @@ import languages from '../config/languages.json';
 import cy from '../assets/translations/cy.json';
 import en from '../assets/translations/en.json';
 import pl from '../assets/translations/pl.json';
+import { Right } from 'native-base';
 
 I18n.translations = { 
   cy: cy, 
@@ -31,35 +32,118 @@ const getTheme = (t) => {
   }
 }
 
+const logo = require('../assets/images/smwcrt_logo.png');
+
+
+
 function MainScreen({ navigation }) { 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.topPanel}>
-        <Text>Test</Text>
+        <ImageBackground 
+          style={styles.topBackground}
+          soruce={require('../assets/images/caverescue.jpg')} 
+          >
+        <View style={styles.infoPanel}>
+          <View style={styles.miniMenu}>
+
+          </View>
+          <View style={styles.dutyPanel}>
+             <View style={styles.dutyInfo}>
+              <Text style={styles.dutyName}>Duty Name</Text>
+              <Text style={styles.positionName}>Position Name</Text>
+             </View>
+             <View style={styles.dutyAction}>
+
+             </View>
+          </View>
+        </View>
+        <View style={styles.orgPanel}>
+          <Image source={logo} style={styles.mainLogo} />
+        </View>
+        </ImageBackground>
+      </View>
+      <View style={styles.searchPanel}>
+        <Text>Search</Text>
+      </View>
+      <View style={styles.actionsPanel}>
+        <Text>Actions</Text>
       </View>
     </SafeAreaView>
   );
 }
 
+const commonStyle = {
+  panel: {
+    borderWidth: 2
+  }
+}
+
 const styles = StyleSheet.create({
     container: { flex: 1 },
-    top_panel: {
-      flex: 4,
-      backgroundColor: 'red',
-      borderWidth: 2
+    topPanel: {
+      ...commonStyle.panel,
+      flex: 5
     },
-
-    buttonView: {
-      width: '40%',
-      height: '40%',
+    topBackground: {
+      flex: 1, 
+      resizeMode: 'cover', 
+      justifyContent: 'center'
     },
-    button: {
-      width: '100%',
-      height: '100%',
-      backgroundColor: colors.primary
+    infoPanel: {
+      ...commonStyle.panel, 
+      flex: 1,
+      flexDirection: 'row'
+    },
+    orgPanel: {
+      ...commonStyle.panel,
+      flex: 4, 
+      
+      alignContent: 'center',
+      justifyContent: 'center'
+    },
+    searchPanel: {
+        ...commonStyle.panel,
+        flex: 1
     }, 
+    actionsPanel: {
+      ...commonStyle.panel,
+      flex: 4
+    },
+    mainLogo: {
+      flex: 1,
+      resizeMode: 'contain', 
+      justifyContent: 'center',
+      alignSelf: 'center',
+      width: '70%'
+    },
     main: {
       backgroundColor: colors.backgroundColor
+    },
+    miniMenu: {
+      flex: 1, 
+      backgroundColor: 'green'
+    },
+    dutyPanel: {
+      flex: 5, 
+      backgroundColor: 'red',
+      flexDirection: 'row'
+    },
+    dutyInfo: {
+      flex: 4,
+      alignItems: 'stretch'
+    },
+    dutyAction: {
+      backgroundColor: 'gold',
+      flex: 1
+    }, 
+    dutyName: {
+      fontSize: 24,
+      textAlign: 'right'
+    }, 
+    positionName: {
+      textAlign: 'right',
+      fontSize: 18
     }
   });
 
